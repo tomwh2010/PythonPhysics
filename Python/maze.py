277b88e8f89d99c8437c124fde5125dfcbc9b60c
tomwh2010 <43741388+tomwh2010@users.noreply.github.com>
2018-11-22 15:12:38 +0100

    maze

---
+++ Python/maze.py
@@ -0,0 +1,62 @@
+##############################################################################
+#Description
+#Maze
+##############################################################################
+
+##############################################################################
+#libraries
+##############################################################################
+import pygame, sys
+from pygame.locals import *
+import twhcolors
+
+##############################################################################
+#constants (variable names written with upper case)
+##############################################################################
+#Frames pr second
+FPS=40
+
+#window size
+WIDTH=800
+HEIGHT=600
+
+##############################################################################
+#variables
+##############################################################################
+
+##############################################################################
+#functions
+##############################################################################
+
+##############################################################################
+#initial code
+##############################################################################
+#initialize the pygame environment
+pygame.init()
+
+# set up the window with size and caption
+screen=pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Maze')
+
+#draw background color to blank the screen
+screen.fill(twhcolors.SILVER)
+
+# creates a clock
+clock=pygame.time.Clock()
+
+calculate()
+##############################################################################
+#main loop
+##############################################################################
+while True:
+    #limit updates to FPS
+    clock.tick(FPS)
+
+    #get events from the event queue
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    #update display
+    pygame.display.flip()
